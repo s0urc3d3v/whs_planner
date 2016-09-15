d53b7d535706c92375f782461d9d7c2bf3442e3e
alexander_bell <alexander_bell@student.wayland.k12.ma.us>
2016-09-15 13:14:49 -0400

    asdf

---
+++ src/main/java/WHS_planner/NewsFeedTest.java
@@ -0,0 +1,10 @@
+package WHS_planner;
+
+/**
+ * Created by alexander_bell on 9/15/16.
+ */
+public class NewsFeedTest {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+}
