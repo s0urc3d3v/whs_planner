3c718a388ab02f3d1858b973f978711bc4cc73a5
john_bachman <jack.github@gmail.com>
2016-09-16 12:04:06 -0400

    Reading for JSON Files, legit

--- src/main/java/WHS_planner/Core/JSON.java
+++ src/main/java/WHS_planner/Core/JSON.java
@@ -3,6 +3,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -17,13 +18,20 @@ public class JSON {
 
     public JSON (String filepath) throws IOException {
         fileWriter = new FileWriter(filepath);
+        try {
+            object =  (JSONObject) parser.parse(new FileReader(filepath));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         object = new JSONObject();
         parser = new JSONParser();
     }
+
     public void writePair(String key, String data){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put(key, data);
     }
+
     public void writeArray(String key, Object data[]){
         key = "@" + key;
         JSONArray jsonArray = new JSONArray();
@@ -32,6 +40,7 @@ public class JSON {
         }
         updateFile();
     }
+
     public void updateFile(){
         try {
             fileWriter.write(object.toJSONString());
@@ -39,5 +48,28 @@ public class JSON {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Object readObject(String key) {
+        return object.get(key);
+    }
+
+    public Object[] readArray(String key) {
+        JSONArray array = (JSONArray) object.get(key);
+        //Turn the JSONArray into an object array
+        int length = array.size();
+        Object objArray[] = new Object[length];
+        for(int i = 0; i < length; i++) {
+            objArray[i] = array.get(i);
+        }
+        return objArray;
+    }
+
+    public Object readPair(String key) {
+        if(key.length() >= 1 && key.substring(0, 1).equals("@")) {
+            //It is an array and needs to be parsed as one.
+            return readArray(key);
+        }
+        return readObject(key);
     }
 }
