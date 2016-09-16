1b2ca08a9ee6e87ff2e1424d2fdff1a64c681159
tzur_almog <tzur.almog@gmail.com>
2016-09-16 11:43:48 -0400

    added day filling algorithim

--- src/main/java/WHS_planner/Calendar/a.java
+++ src/main/java/WHS_planner/Calendar/a.java
@@ -4,4 +4,23 @@ package WHS_planner.Calendar;
  * Created by matthewelbing on 16.09.16.
  */
 public class a {
+    public static void main(String[] args) {
+        int calendar[][] = new int[5][7];
+        int startDay;
+        int DayInMonth;
+
+        for (int r = 0; r < 5 ; r++) {
+            for (int c = 0; c < 7; c++) {
+                if (r == 1) {
+                    c = startDay;
+
+                }
+                System.out.println(""+DayInMonth);
+                DayInMonth ++;
+            }
+
+        }
+
+        }
+    }
 }
