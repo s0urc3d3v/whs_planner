e6b2182b1c4f3d6934e9264b1caf4e8d087f46c8
john_bachman <jack.github@gmail.com>
2016-09-16 11:05:48 -0400

    JSON Constructor

--- src/main/java/WHS_planner/Core/JSON.java
+++ src/main/java/WHS_planner/Core/JSON.java
@@ -2,12 +2,20 @@ package WHS_planner.Core;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created by matthewelbing on 16.09.16.
  */
 public class JSON {
-    public JSON (){
 
+    FileWriter fileWriter;
+    JSONObject object;
+    JSONParser parser;
+
+    public JSON (String filepath) throws IOException {
+        fileWriter = new FileWriter(filepath);
     }
     public void writePair(String key, String data){
         JSONObject jsonObject = new JSONObject();
