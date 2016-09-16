6425be1514b61a33fffd135d728487be36e28a0c
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2016-09-16 17:42:55 -0400

    add test path for JSON database

---
+++ src/main/java/WHS_planner/CoreUI/SimpleController.java
@@ -0,0 +1,23 @@
+package WHS_planner.CoreUI;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SimpleController implements Initializable {
+
+    @FXML
+    private JFXButton buttonTest;
+
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        buttonTest.setText("large dICKed man");
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
+
+
+}
