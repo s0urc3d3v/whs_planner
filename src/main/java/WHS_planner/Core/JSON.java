2e37f4879a545f87ba2198f3c4057182445a26b3
Matthew Elbing <author@techredesign.net>
2016-09-16 11:02:18 -0400

    Json write functions

--- src/main/java/WHS_planner/Core/JSON.java
+++ src/main/java/WHS_planner/Core/JSON.java
@@ -1,8 +1,19 @@
 package WHS_planner.Core;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 /**
  * Created by matthewelbing on 16.09.16.
  */
 public class JSON {
+    public JSON (){
 
+    }
+    public void writePair(String key, String data){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(key, data);
+    }
+    public void writeJsonArray(String key, String data){
+
+    }
 }
