fcb0f3b1fbf7ffbdc83cb49d0de3618d5d7777c4
John Broderick <johnbroderick12@gmail.com>
2016-09-16 11:59:03 -0400

    a

---
+++ src/main/java/WHS_planner/Schedule/Schedule.java
@@ -0,0 +1,28 @@
+package WHS_planner.Schedule;
+
+import javafx.*;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+
+
+public class Schedule
+{
+    public Schedule()
+    {
+
+    }
+
+
+
+    public Canvas drawSchedule(int w, int h)
+    {
+        Canvas canvas = new Canvas(w, h);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+
+
+        return canvas;
+    }
+
+}
