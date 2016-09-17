a735eec7dca1d0ed398a3f1e67ee304666ee8c7f
Matthew Elbing <author@techredesign.net>
2016-09-17 09:53:03 -0400

    Add feed retrieving library

---
+++ src/main/java/WHS_planner/Core/ErrorHandler.java
@@ -0,0 +1,13 @@
+package WHS_planner.Core;
+
+/**
+ * Created by matthewelbing on 17.09.16.
+ */
+public class ErrorHandler {
+    public ErrorHandler(){
+
+    }
+    public static void handleGenericError(){
+        System.out.println("PLACEHOLDER");
+    }
+}
