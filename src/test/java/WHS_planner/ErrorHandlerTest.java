94c63b2666dc920ab392471ed42e9f5d731bdc12
Matthew Elbing <author@techredesign.net>
2016-09-21 11:57:30 -0400

    Add errorHandler test cases

---
+++ src/test/java/WHS_planner/ErrorHandlerTest.java
@@ -0,0 +1,13 @@
+package WHS_planner;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by matthewelbing on 21.09.16.
+ */
+public class ErrorHandlerTest extends TestCase{
+
+    public void testCreateErrorLog() throws Exception {
+
+    }
+}
