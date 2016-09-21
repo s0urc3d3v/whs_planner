d5aafe4864174d401a0cd2f79c3d664978c622dd
Sharqosity <typingpalguy@gmail.com>
2016-09-21 11:52:43 -0400

    html parser skeleton and imported jsoup

---
+++ src/main/java/WHS_planner/News/read/HTMLParser.java
@@ -0,0 +1,28 @@
+package WHS_planner.News.read;
+
+/**
+ * Created by george_jiang on 9/20/16.
+ */
+//import java.lang.Object;
+
+import org.jsoup.Jsoup;
+import WHS_planner.News.model.Feed;
+import WHS_planner.News.model.FeedMessage;
+import WHS_planner.News.read.RSSFeedParser;
+
+public class HTMLParser {
+    String y = new String();
+
+    public HTMLParser()
+    {
+
+    }
+    private String Escape(String x)
+    {
+        y = Jsoup.parse(x).text();
+        return y;
+    }
+
+
+
+}
