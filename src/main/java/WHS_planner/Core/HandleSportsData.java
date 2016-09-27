be7143157fc25a3b959fe566b45c139d23986ec2
Matthew Elbing <author@techredesign.net>
2016-09-27 12:45:13 -0400

    Add IO partial support

---
+++ src/main/java/WHS_planner/Core/HandleSportsData.java
@@ -0,0 +1,13 @@
+package WHS_planner.Core;
+
+/**
+ * Created by matthewelbing on 27.09.16.
+ */
+public class HandleSportsData {
+    private String raw_data = "";
+    public HandleSportsData(){
+        //raw_data = CoreUI.getSportsData();
+    }
+
+
+}
