027b5c8a2a00ec728e9ef34a36c47bc98d04f942
John Broderick <johnbroderick12@gmail.com>
2016-09-27 12:43:57 -0400

    renamed a few files
    Still memes/10

--- src/main/java/WHS_planner/Schedule/scheduleParse.java
+++ src/main/java/WHS_planner/Schedule/scheduleParse.java
@@ -8,22 +8,25 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 
-/**
- * Created by william_robison on 9/26/16.
- */
-public class wankers {
+public class scheduleParse {
 
 
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args)
+    {
+        try
+        {
             getClasses();
-        } catch (IOException e) {
+        }
+
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public static void getClasses() throws IOException {
+    public static void getClasses() throws IOException
+    {
         int[] wanks = new int[12];
         int count = 0;
         Element schedElement = null;
@@ -32,7 +35,8 @@ public class wankers {
         Document doc = Jsoup.parse(input,"UTF-8","");
         Elements links  = doc.select("table");
 
-        for (int i = 0; i < links.size(); i++) {
+        for (int i = 0; i < links.size(); i++)
+        {
             if(links.get(i).id().equalsIgnoreCase("Student's Schedule")){
                 schedElement = links.get(i);
                 System.out.println(i);
