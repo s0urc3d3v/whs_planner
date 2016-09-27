fe8fa5c2528b11c694b6b3a8ba9bb11709c607da
Matthew Elbing <author@techredesign.net>
2016-09-27 13:05:27 -0400

    Add IO api support for schedule team.  See API guide

--- src/main/java/WHS_planner/Core/IO.java
+++ src/main/java/WHS_planner/Core/IO.java
@@ -3,6 +3,7 @@ package WHS_planner.Core;
 import org.json.simple.JSONArray;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by matthewelbing on 27.09.16.
@@ -12,15 +13,10 @@ public class IO {
     public IO(){
         jsonApi = new JSON();
     }
-    public void writeScheduleArray(String[][] classLayout){
-        JSONArray baseJsonArray = new JSONArray();
-        for (int i = 0; i < classLayout.length; i++) {
-            JSONArray secondaryJsonArray = new JSONArray();
-            for (int j = 0; j < classLayout[0].length; j++) {
-                secondaryJsonArray.add(classLayout[i][j]);
-            }
-            baseJsonArray.add(secondaryJsonArray);
-        }
-        jsonApi.writePair("schedule", baseJsonArray.toJSONString());
+    public void writeScheduleArray(Object[] classLayout){
+        jsonApi.writeArray("schedule", classLayout);
+    }
+    public Object[] readScheduleArray(){
+        return (Object[]) jsonApi.readPair("@schedule");
     }
 }
