beaa721b5d5978f6f314d17c2d898ffe37ed1fd4
Matthew Elbing <author@techredesign.net>
2016-09-28 11:35:59 -0400

    This is a commit

---
+++ src/test/java/WHS_planner/AutoSaveTest.java
@@ -0,0 +1,14 @@
+package WHS_planner;
+
+import WHS_planner.Core.AutoSave;
+import junit.framework.TestCase;
+
+/**
+ * Created by matthewelbing on 28.09.16.
+ */
+public class AutoSaveTest extends TestCase {
+    public void testAutoSave(){
+        AutoSave autoSave = new AutoSave(100,);
+        autoSave.run();
+    }
+}
