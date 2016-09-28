da85061673841843fc38eef38747ccaae8c89a5d
william_robison <willrobison347@gmail.com>
2016-09-28 07:22:05 -0400

    extract schedule information nerds

---
+++ src/main/java/WHS_planner/Schedule/ScheduleDay.java
@@ -0,0 +1,7 @@
+package WHS_planner.Schedule;
+
+/**
+ * Created by william_robison on 9/28/16.
+ */
+public class ScheduleDay {
+}
--- src/main/java/WHS_planner/Schedule/scheduleParse.java
+++ src/main/java/WHS_planner/Schedule/scheduleParse.java
@@ -24,37 +24,78 @@ public class scheduleParse {
     }
 
     public static void getClasses() throws IOException {
-        int[] wanks = new int[12];
-        int count = 0;
+        int count = 2;
         Element schedElement = null;
+        Element rawClass = null;
+        Elements classesRaw = new Elements();
 
         File input = new File("raw.html");
         Document doc = Jsoup.parse(input,"UTF-8","");
         Elements tables  = doc.select("table");
 
-        for (int i = 0; i < tables.size(); i++) {
+        for (int i = 0; i < tables.size(); i++) { // Finds the Schedule table & assigns it to schedElement
             if(tables.get(i).id().equalsIgnoreCase("Student's Schedule")){
                 schedElement = tables.get(i);
-                System.out.println(i);
             }
         }
 
-//        schedElement = schedElement.select("table table").first();
-        while(! schedElement.select(":has(table)").isEmpty()){
-            schedElement = schedElement.select("table table").first();
+
+            while (!schedElement.select(":has(table)").isEmpty()) {
+                if(count < 59){
+                rawClass = schedElement.select("table table").get(count);
+                count++;
+                classesRaw.add(rawClass);}
+                else {break;}
+
+            }
+
+        for (int i = 0; i < 55; i++) {
+            System.out.println(polishClass(classesRaw.get(i)));
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println(classesRaw.get(i));
+            System.out.println();
+            System.out.println();
+            System.out.println();
         }
 
-//        System.out.println(wowo);
-        System.out.println(schedElement);
-        System.out.println(count);
-        String wow2 = schedElement.toString();
-//        System.out.println(wow2);
 
     }
 
-    private static String[] findClass(int period, String el){
+    private static String polishClass(Element el){ // returns "Class:Teacher:Roome:Period"
+        //Write code for advisory
+        String strEl = el.toString();
+        String polishedStr = "";
+        String utilStr = "";
 
-        return null;
+
+        if(strEl.length() < 200){
+            return "free";
+        }
+
+        //Find td's
+
+        //removes first garbage
+        strEl = strEl.substring(0,strEl.indexOf("<td")) + strEl.substring(strEl.indexOf("</td>") + 5);
+
+        //Finds Teacher
+        for (int i = 0; i < 4; i++) {
+            if(i == 3){
+                utilStr = strEl.substring(strEl.indexOf("<td") + 1, strEl.indexOf("</td>") + 3);
+                polishedStr += utilStr.substring(utilStr.indexOf(":") + 2, utilStr.indexOf(" </"));
+                strEl = strEl.substring(0, strEl.indexOf("<td")) + strEl.substring(strEl.indexOf("</td>") + 5);
+            }else {
+                utilStr = strEl.substring(strEl.indexOf("<td") + 1, strEl.indexOf("</td>") + 3);
+                polishedStr += utilStr.substring(utilStr.indexOf("> ") + 2, utilStr.indexOf(" </")) + ":";
+                strEl = strEl.substring(0, strEl.indexOf("<td")) + strEl.substring(strEl.indexOf("</td>") + 5);
+            }
+        }
+
+
+        //
+
+            return polishedStr;
+        }
     }
 
-}
