c0a22f8ecd7fd9a3d71c870dcab0f89f076c6e39
Matthew Elbing <author@techredesign.net>
2016-10-06 08:14:04 -0400

    fix OAth stuffs

--- src/main/java/WHS_planner/Core/Gmail.java
+++ src/main/java/WHS_planner/Core/Gmail.java
@@ -1,14 +1,26 @@
 package WHS_planner.Core;
 
+
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+
 import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.*;
+import com.google.api.services.gmail.Gmail;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,7 +38,7 @@ public class Gmail {
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static HttpTransport HTTP_TRANSPORT;
 
-    private static final List<String> SCOPES = Arrays.asList(GmailScopes.GMAIL_LABELS, GmailScopes.GMAIL_COMPOSE, GmailScopes.GMAIL_SEND, GmailScopes.MAIL_GOOGLE_COM);
+    private static final List<String> SCOPES = Arrays.asList(GmailScopes.GMAIL_COMPOSE, GmailScopes.GMAIL_SEND, GmailScopes.GMAIL_READONLY);
     static {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -44,8 +56,21 @@ public class Gmail {
                 .setAccessType("offline")
                 .build();
         Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver()).authorize("user");
-        )
+                flow, new LocalServerReceiver()).authorize("user"));
+        System.out.println("Creditials saved to: " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+
+    }
+    public static Gmail getGmailService() throws IOException {
+        Credential credential = authorize();
+        return new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplication(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void auth() throws IOException{
+        Gmail service = getGmailService();
+        ListLabelsResponse listResponse = service.users()
     }
 
 
