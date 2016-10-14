cae36603eef164546861f43de11beef18e5463f7
Matthew Elbing <author@techredesign.net>
2016-10-14 11:01:19 -0400

    merge conflict no more

--- src/main/java/WHS_planner/Main.java
+++ src/main/java/WHS_planner/Main.java
@@ -8,5 +8,6 @@ public class Main
     public static void main(String[] args)
     {
         MeetingFileHandler.configFileHandler(); //This must be at the top of Main() for meeting import to work
+
     }
 }
