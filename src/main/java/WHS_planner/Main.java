00e41d9d25562ffa66fcacd1b0a0332e627c7b50
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2016-10-16 10:36:46 -0400

    I cant figure out why I cant call your method

--- src/main/java/WHS_planner/Main.java
+++ src/main/java/WHS_planner/Main.java
@@ -8,16 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    static String argsGlobal[];
     public static void main(String[] args)
     {
+        argsGlobal = args;
        /*if (System.getProperty("os.name").toLowerCase().contains("mac") || System.getProperty("os.name").toLowerCase().contains("os x")){
         }
         MeetingFileHandler.configFileHandler(); //This must be at the top of Main() for meeting import to work
-        launch(args); */
+         */
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        launch(argsGlobal);
         NavigationBar bar = new NavigationBar();
         bar.start(primaryStage);
     }
