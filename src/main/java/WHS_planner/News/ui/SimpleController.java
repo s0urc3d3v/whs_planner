b22089891305a728f708f0ab21a3ae5537993221
Sharqosity <typingpalguy@gmail.com>
2016-10-21 18:29:15 -0400

    Everything works. Nothing doesn't work.

--- src/main/java/WHS_planner/News/ui/SimpleController.java
+++ src/main/java/WHS_planner/News/ui/SimpleController.java
@@ -21,6 +21,18 @@ import java.util.ResourceBundle;
 
 public class SimpleController implements Initializable {
 
+    private final int ZERO = 0;
+    private final int ONE = 1;
+    private final int TWO = 2;
+    private final int THREE = 3;
+    private final int FOUR = 4;
+    private final int FIVE = 5;
+    private final int SIX = 6;
+    private final int SEVEN = 7;
+    private final int EIGHT = 8;
+    private final int NINE = 9;
+    private final int TEN = 10;
+
     @FXML
     private JFXListView<VBox> articleListView;
 
@@ -33,14 +45,7 @@ public class SimpleController implements Initializable {
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         articleList = FXCollections.observableArrayList();
 
-        JFXButton refreshButton = new JFXButton("Refresh");
-        refreshButton.setOnAction((event) -> {
-            updateFrame();
-        });
-
-        VBox r = new VBox(refreshButton);
-        articleList.add(r);
-        articleListView.setItems(articleList);
+//        articleListView.setItems(articleList);
         updateFrame();
     }
 
@@ -53,30 +58,45 @@ public class SimpleController implements Initializable {
         }
     }
 
-
     @FXML
     private void updateFrame() {
-//        System.out.println("Update frame called!");
+        articleList.clear();
 
-//        for (FeedMessage message : feed.getMessages()) {
-//            VBox v = new VBox(new Hyperlink(message.getTitle()), new Label(message.getAuthor()), new Label(message.getDescription()));
-//            articleList.add(v);
-//        }
-        //TODO make it so it replaces articles instead of adding
+        JFXButton refreshButton = new JFXButton("Refresh");
+        refreshButton.setOnAction((event) -> updateFrame());
+        VBox r = new VBox(refreshButton);
+        articleList.add(r);
 
         for (int i = 0; i < feedArray.size(); i++) {
-
             Hyperlink hpl = new Hyperlink(feedArray.get(i).getTitle());
-            hpl.setOnAction((event) -> {
-                openLink(i);
-                //TODO how
-            });
-
+            if (i == 0) {
+                hpl.setOnAction((event) -> openLink(ZERO));
+            } else if (i == 1) {
+                hpl.setOnAction((event) -> openLink(ONE));
+            } else if (i == 2) {
+                hpl.setOnAction((event) -> openLink(TWO));
+            } else if (i == 3) {
+                hpl.setOnAction((event) -> openLink(THREE));
+            } else if (i == 4) {
+                hpl.setOnAction((event) -> openLink(FOUR));
+            } else if (i == 5) {
+                hpl.setOnAction((event) -> openLink(FIVE));
+            } else if (i == 6) {
+                hpl.setOnAction((event) -> openLink(SIX));
+            } else if (i == 7) {
+                hpl.setOnAction((event) -> openLink(SEVEN));
+            } else if (i == 8) {
+                hpl.setOnAction((event) -> openLink(EIGHT));
+            } else if (i == 9) {
+                hpl.setOnAction((event) -> openLink(NINE));
+            } else if (i == 10) {
+                hpl.setOnAction((event) -> openLink(TEN));
+            } else {
+                System.out.println("lol something went wrong");
+            }
             VBox v = new VBox(hpl, new Label(feedArray.get(i).getAuthor()), new Label(feedArray.get(i).getDescription()));
             articleList.add(v);
         }
         articleListView.setItems(articleList);
-
-
     }
 }
