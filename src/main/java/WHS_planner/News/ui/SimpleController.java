60ff7b6e3602e436bd041f57a6545c48f6eecda2
Sharqosity <typingpalguy@gmail.com>
2016-10-25 08:59:33 -0400

    Images work. Refresh takes like 10 seconds though.

--- src/main/java/WHS_planner/News/ui/SimpleController.java
+++ src/main/java/WHS_planner/News/ui/SimpleController.java
@@ -21,6 +21,7 @@ import javafx.scene.layout.VBox;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -34,10 +35,11 @@ public class SimpleController implements Initializable {
     private HTMLScanner HTMLScanner = new HTMLScanner();
     private ObservableList<VBox> articleList = FXCollections.observableArrayList();
     private URL url;
-
+    private URL kms;
 
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
         updateFrame();
+
     }
 
     @FXML
@@ -50,52 +52,78 @@ public class SimpleController implements Initializable {
     }
 
     @FXML
-    private void updateFrame() throws IOException {
+    private void updateFrame() /*throws IOException*/ {
         articleList.clear();
 
         JFXButton refreshButton = new JFXButton("Refresh");
         refreshButton.getStyleClass().add("button-raised");
+
         refreshButton.setOnAction((event) -> updateFrame());
         VBox r = new VBox(refreshButton);
         articleList.add(r);
 
         for (int i = 0; i < feedArray.size(); i++) {
-            url = new URL(HTMLScanner.scanHTML(HTMLScanner.scanURL(new URL(feedArray.get(i).getLink()))));
 
-            BufferedImage bf = null;
-            try {
-                bf = ImageIO.read(url);
-            } catch (IOException ex) {
-            }
 
-            WritableImage wr = null;
-            if (bf != null) {
-                wr = new WritableImage(bf.getWidth(), bf.getHeight());
-                PixelWriter pw = wr.getPixelWriter();
-                for (int x = 0; x < bf.getWidth(); x++) {
-                    for (int y = 0; y < bf.getHeight(); y++) {
-                        pw.setArgb(x, y, bf.getRGB(x, y));
-                    }
-                }
-            }
 
-            ImageView img = new ImageView(wr);
-            img.setImage(wr);
+
 
             final int eye = i;
             Hyperlink hpl = new Hyperlink(feedArray.get(i).getTitle());
             hpl.setOnAction((event) -> openLink(eye));
             hpl.setPadding(new Insets(0, 0, 0, -1));
-            Label author = new Label(feedArray.get(i).getAuthor());
-            author.setWrapText(true);
             Label description = new Label(feedArray.get(i).getDescription());
             description.setWrapText(true);
-            VBox v = new VBox(hpl, author, description);
-            v.setMaxWidth(550);
-            //TODO ^don't hard code this
-            articleList.add(v);
+
+
+            try {
+                if (HTMLScanner.scanHTML(HTMLScanner.scanURL(new URL(feedArray.get(i).getLink()))) == null) {
+                    VBox v = new VBox(hpl, /*author,*/ description);
+                    v.setMaxWidth(550);
+                    articleList.add(v);
+                } else {
+                    url = new URL(HTMLScanner.scanHTML(HTMLScanner.scanURL(new URL(feedArray.get(i).getLink()))));
+
+
+                    BufferedImage bf = null;
+                    try {
+                        bf = ImageIO.read(url);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    WritableImage wr = convertImg(bf);
+
+
+                    ImageView img = new ImageView(wr);
+                    img.setImage(wr);
+
+
+                    VBox v = new VBox(img, hpl, /*author,*/ description);
+                    v.setMaxWidth(550);
+                    articleList.add(v);
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
         }
 
         articleListView.setItems(articleList);
     }
+
+    private WritableImage convertImg(BufferedImage bf) {
+        WritableImage wr = null;
+        if (bf != null) {
+            wr = new WritableImage(bf.getWidth(), bf.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < bf.getWidth(); x++) {
+                for (int y = 0; y < bf.getHeight(); y++) {
+                    pw.setArgb(x, y, bf.getRGB(x, y));
+                }
+            }
+        }
+        return wr;
+    }
 }
+
