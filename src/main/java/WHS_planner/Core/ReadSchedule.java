3d24475a7bd1d49ae5804a6224e9acd4fa0f472a
john_bachman <jack.github@gmail.com>
2016-11-07 18:43:34 -0500

    Headless Driver Now Different

--- src/main/java/WHS_planner/Core/ReadSchedule.java
+++ src/main/java/WHS_planner/Core/ReadSchedule.java
@@ -1,11 +1,22 @@
 package WHS_planner.Core;
 
+import jdk.jfr.events.FileWriteEvent;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.thoughtworks.selenium.SeleneseTestBase.fail;
@@ -19,72 +30,181 @@ public class ReadSchedule {
     private String username;
     private String password;
     private StringBuffer verificationErrors = new StringBuffer();
-    private HeadlessWebDriver hWebDriver;
     private boolean acceptNextAlert = true;
     private String pageSource;
-    public ReadSchedule (){
+    public ReadSchedule (){}
 
-    }
+    private HttpURLConnection connection;
+    private List<String> cookies;
+    private final String USER_AGENT = "Mozilla/5.0";
+
     public void authAndFindTableWithIpass(String user, String pass) throws Exception {
-        //Auth and navigate to schedule
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("window-size=1024,768");
-        hWebDriver = new HeadlessWebDriver();
-        if(hWebDriver.setup()) {
-            String baseUrl = "https://ipass.wayland.k12.ma.us";
-            hWebDriver.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            hWebDriver.driver.get(baseUrl + "/school/ipass/syslogin.html");
-            hWebDriver.driver.findElement(By.name("userid")).clear();
-            hWebDriver.driver.findElement(By.name("userid")).sendKeys(user);
-            hWebDriver.driver.findElement(By.name("password")).clear();
-            hWebDriver.driver.findElement(By.name("password")).sendKeys(pass);
-            //hWebDriver.driver.findElement(By.cssSelector("img[alt=\"Login to iPass\"]")).submit();
-            hWebDriver.driver.findElement(By.name("password")).sendKeys(Keys.TAB);
-            hWebDriver.driver.executeScript("javascript:document.login.submit()"); //working just needed a parenthesis
-            //hWebDriver.driver.get(baseUrl + "https://ipass.wayland.k12.ma.us/school/ipass/index.html?dt=09261636690");
-            hWebDriver.driver.get("https://ipass.wayland.k12.ma.us/school/ipass/samschedule.html?m=506&amp;pr=19&amp;dt=09261649872");
-            pageSource = hWebDriver.driver.getPageSource();
-            Files.write(Paths.get(System.getProperty("user.dir") + File.separator + "raw.html"), pageSource.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            hWebDriver.close();
-            String verificationErrorString = verificationErrors.toString();
-            if (!"".equals(verificationErrorString)) {
-                fail(verificationErrorString);
+        String url = "https://ipass.wayland.k12.ma.us/school/ipass/syslogin.html";
+        CookieHandler.setDefault(new CookieManager());
+        CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        Grabber grabber = new Grabber();
+        try {
+            String page = grabber.getPageContent(url);
+            String params = grabber.getForm(page, user, pass);
+            grabber.send(url, params);
+            //String res = grabber.getPageContent("https://ipass.wayland.k12.ma.us/school/ipass/samschedule.html");
+            File output = new File("raw.html");
+            FileWriter fileWriter = new FileWriter(output);
+            fileWriter.write(grabber.getPageContent("https://ipass.wayland.k12.ma.us/school/ipass/samschedule.html?m=506&amp;pr=19&amp;dt=09261649872"));
+            connection.disconnect();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    protected class Grabber
+    {
+        private Grabber()
+        {
+
+        }
+
+        private String getPageContent(String url) throws Exception
+        {
+            URL ipass = new URL(url);
+            connection = (HttpsURLConnection) ipass.openConnection();
+            connection.setRequestMethod("GET");
+
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+
+            connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            connection.setRequestProperty("Accept-Encoding", "gzip, deflate, sdch, br");
+
+            if (cookies != null)
+            {
+                for (String cookie : cookies)
+                {
+                    connection.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
+                }
             }
-        } else {
-            ErrorHandler.handleGenericError("WebDriver failed to initialize", new Exception());
-        }
-    }
-    private boolean isElementPresent(By by) {
-        try {
-            hWebDriver.driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
 
-    private boolean isAlertPresent() {
-        try {
-            hWebDriver.driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+            int resp = connection.getResponseCode();
 
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = hWebDriver.driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
+            System.out.println("\nSending GET request to " + url);
+            System.out.println("Response code is: "+resp);
+
+            InputStreamReader ir = new InputStreamReader(connection.getInputStream());
+            BufferedReader br = new BufferedReader(ir);
+
+            String inLine;
+            StringBuilder response = new StringBuilder();
+
+            while((inLine = br.readLine()) != null)
+            {
+                response.append(inLine);
+                response.append("\n");
             }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
+            br.close();
+            ir.close();
+
+            return response.toString();
+        }
+
+        private String getForm(String html, String user, String pass) throws Exception
+        {
+            System.out.println("Getting form data...");
+
+            Document doc = Jsoup.parse(html);
+
+            Element loginform = doc.getElementById("login");
+            Elements inputelements = loginform.getElementsByTag("input");
+
+            ArrayList<String> params = new ArrayList<String>();
+
+            for(Element el : inputelements)
+            {
+                String key = el.attr("name");
+                String val = el.attr("value");
+
+                if(key.equals("userid"))
+                {
+                    val = user;
+                }
+                else if(key.equals("password"))
+                {
+                    val = pass;
+                }
+
+                params.add(key + "=" + URLEncoder.encode(val, "UTF-8"));
+            }
+
+            StringBuffer buffer = new StringBuffer();
+
+            for(String param : params)
+            {
+                if(buffer.length() == 0)
+                {
+                    buffer.append(param);
+                }
+                else
+                {
+                    buffer.append("&" + param);
+                }
+            }
+
+            return buffer.toString();
+        }
+
+        private void send(String url, String params) throws Exception
+        {
+            System.out.println("Attempting to send data");
+
+            URL obj = new URL(url);
+
+            connection = (HttpURLConnection) obj.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+            connection.setRequestProperty("Connection", "keep-alive");
+            connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            connection.setRequestProperty("Accept-Encoding", "gzip, deflate, sdch, br");
+
+
+
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+
+            dos.writeBytes(params);
+            dos.flush();
+            dos.close();
+
+
+            int response = connection.getResponseCode();
+
+            System.out.println("\nSending POST request to "+url);
+            System.out.println("Parameters : " + params);
+            System.out.println("Response code:" + response);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String input;
+            StringBuffer stringbuffer = new StringBuffer();
+
+            while((input = br.readLine()) != null)
+            {
+                stringbuffer.append(input);
+            }
+            br.close();
+
+        }
+
+        private List<String> getCookies()
+        {
+            return cookies;
+        }
+
+        private void setCookies(List<String> cs)
+        {
+            cookies = cs;
         }
     }
-
 }
--- src/main/java/WHS_planner/Core/SportsHandler.java
+++ src/main/java/WHS_planner/Core/SportsHandler.java
@@ -10,49 +10,20 @@ import java.util.List;
 /**
  * Created by matthewelbing on 27.09.16.
  */
-public class SportsHandler extends HeadlessWebDriver {
+public class SportsHandler {
 
     //GLITCH LINK: http://miaa.net/athletics/calendar/schedule.php?school_id=1424&team_type_ids=45&PRINT_PREVIEW=1
     String sportSite = "http://miaa.net/schools/public/WaylWa1";
 
     public SportsHandler() {
         super();
-        //Setup and Initialize the Driver
-        if(setup()) {
-            //If setup of the driver worked than load this link
-            driver.get(sportSite);
-        } else {
-            ErrorHandler.handleGenericError("HeadlessWebDriver in SportsHandler decided to fail :( ", new Exception());
-        }
     }
 
     public String[] getSports() {
-        //Find all the sports name boxes
-        WebElement tableBody = driver.findElements(By.className("x-grid3-body")).get(1);
-        //Get the rows that the names are in
-        List<WebElement> tableRows = tableBody.findElements(By.className("x-grid3-row-table"));
-        //Make a string array because you want to
-        String[] sports = new String[tableRows.size()];
-        //Go through the rows
-        for(int i = 0; i < tableRows.size(); i++) {
-            //Pull out the second table element... that is the sports name
-            sports[i] = tableRows.get(i).findElements(By.tagName("td")).get(1).getText();
-        }
-        //Return over-sized array
-        return sports;
+        return null;
     }
 
     public String[] getEvents(int sportIndex) {
-        //TODO: This methods does not work yet
-        WebElement tableBody = driver.findElements(By.className("x-grid3-body")).get(1);
-        List<WebElement> tableRows = tableBody.findElements(By.className("x-grid3-row-table"));
-        WebElement team = tableRows.get(sportIndex);
-        team.findElement(By.className("x-grid3-row-checker"));
-        team.click();
-        team.click();
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        WebElement element = webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("schedule-table")));
-//        System.out.println(driver.findElementByClassName("schedule-table").findElements(By.tagName("tr")).get(4).findElements(By.tagName("td")).get(1).getText());
         return null;
     }
 }
