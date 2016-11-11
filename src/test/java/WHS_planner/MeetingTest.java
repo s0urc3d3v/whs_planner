f93357828a0a12efd0dc34e9582d1e41b211b655
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2016-11-11 10:35:27 -0500

    fix travis

--- src/test/java/WHS_planner/MeetingTest.java
+++ src/test/java/WHS_planner/MeetingTest.java
@@ -1,8 +1,6 @@
 package WHS_planner;
 
-import WHS_planner.Core.GmailApiAccess;
 import WHS_planner.Core.Meeting;
-import WHS_planner.Core.ReadSchedule;
 import WHS_planner.Core.Student;
 import WHS_planner.Util.Course;
 import junit.framework.TestCase;
@@ -15,7 +13,7 @@ public class MeetingTest extends TestCase {
         Course english = new Course("English", 4, "Teacher", Course.level.COLLEGE);
         Student requestingStudent = new Student("John", "Smith", "test@test.com", 12, "teacher1");
         Student studentRequested = new Student("Smith", "John", "matthewelbing@gmail.com", 12, "teacher2");
-        Meeting m = new Meeting(requestingStudent, studentRequested, 10, 12, english);
+        Meeting m = new Meeting(requestingStudent, studentRequested, 11, 13, 10, 12, 16, english);
         m.create();
         ReadMeetingDataTest readMeetingDataTest = new ReadMeetingDataTest();
         readMeetingDataTest.testReadMeetingData();
