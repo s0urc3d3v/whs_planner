bb83e10f56c2268f96c5e96aa3b14b7a611d6147
matthewelbing <author@techredesign.net>
2016-12-02 13:07:04 -0500

    add build product script and modified pom

--- build_production.sh
+++ build_production.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mvn install
-mvn compiler:compile
-mvn org.apache.maven.plugins:maven-compiler-plugin:compile
-mvn org.apache.maven.plugins:maven-compiler-plugin:2.0.2:compile
+mvn install -DskipTests
+mvn compiler:compile -DskipTests
+mvn org.apache.maven.plugins:maven-compiler-plugin:compile -DskipTests
+mvn org.apache.maven.plugins:maven-compiler-plugin:2.0.2:compile -DskipTests
+mvn package -DskipTests
