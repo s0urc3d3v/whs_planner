d176eb057e3050768ddb520d8e71f38829ef5389
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2016-12-04 10:00:51 -0500

    build fixes and general cleanup of code

--- src/main/java/WHS_planner/Main.java
+++ src/main/java/WHS_planner/Main.java
@@ -6,7 +6,6 @@ import WHS_planner.CoreUI.NavigationBar;
 import WHS_planner.Util.MemoryCredentials;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.apache.log4j.PropertyConfigurator;
 
 import java.io.File;
 
@@ -16,12 +15,8 @@ public class Main extends Application
 
     public static void main(String[] args)
     {
-       /*if (System.getProperty("os.name").toLowerCase().contains("mac") || System.getProperty("os.name").toLowerCase().contains("os x")){
-        }
-        MeetingFileHandler.configFileHandler(); //This must be at the top of Main() for meeting import to work
-         */
+
        MemoryCredentials memoryCredentials = new MemoryCredentials(null, null, null);
-       PropertyConfigurator.configure("log4j.properties");
 
        launch(args);
         IO io = new IO("src" + File.separator + "main" + File.separator + "resources"+ File.separator + "Core" + File.separator + "util.json");
