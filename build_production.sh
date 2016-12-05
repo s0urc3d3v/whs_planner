84dae011d8ce21167176faec6e2adc425eff4132
matthewelbing <author@techredesign.net>
2016-12-05 11:42:03 -0500

    semi working build

--- build_production.sh
+++ build_production.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#DONT USE THIS IT DOES NOT CREATE THE CORRECT BUILD
 mvn install -DskipTests
 mvn compiler:compile -DskipTests
 mvn org.apache.maven.plugins:maven-compiler-plugin:compile -DskipTests
