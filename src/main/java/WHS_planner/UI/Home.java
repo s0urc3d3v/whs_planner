8039859b30079ee518809fa585d2d4b4a13ee8d1
Sharqosity <typingpalguy@gmail.com>
2016-12-06 22:39:02 -0500

    fml

--- src/main/java/WHS_planner/UI/Home.java
+++ src/main/java/WHS_planner/UI/Home.java
@@ -14,7 +14,6 @@ class Home extends BorderPane {
     private Pane cal;
     private ScrollPane news;
     private Pane bar;
-    private ScrollPane newsScroll;
 
 
     Home(Pane calendar, Pane newsUI, Pane schedule) {
@@ -22,40 +21,36 @@ class Home extends BorderPane {
 
 
         this.newsUI = newsUI;
-        newsScroll = new ScrollPane();
+        ScrollPane newsScroll = new ScrollPane();
         newsScroll.setContent(newsUI);
         newsScroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         newsScroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         newsScroll.setFitToWidth(true);
         newsScroll.setStyle("-fx-background-color: #FFFFFF;");
         newsScroll.getStyleClass().setAll("scroll-bar");
-
-//        newsScroll.prefWidthProperty().bind(this.widthProperty());
-//        newsScroll.prefHeightProperty().bind(this.heightProperty());
-
         newsScroll.getStylesheets().add("/UI/NewsUI.css");
-
-
         news = newsScroll;
+
+
         bar = schedule;
         initialize();
     }
 
     private void initialize() {
-//        this.set
         this.setPrefSize(640, 480);
         //TODO ^
         this.setCenter(cal);
 
 
-        setBottom(bar);
-        bar.setMaxHeight(30);
-        bar.setPadding(new Insets(5, 0, 5, 0));
-        //Horizontal insets don't work...
         news.setPrefWidth(280);
         news.setMaxWidth(280);
         news.setPrefHeight(this.getPrefHeight());
         this.setRight(news);
+
+
+        setBottom(bar);
+        bar.setMaxHeight(30);
+        bar.setPadding(new Insets(5, 0, 5, 0));
     }
 
 
