796a15fb3a51c55aacb96c01bfc902e7bebbfb61
matthewelbing <author@techredesign.net>
2016-12-13 13:32:04 -0500

    fix

--- on_success.sh
+++ on_success.sh
@@ -1,18 +1,1 @@
-Matthews-MacBook-Pro:whs_planner matthewelbing$ mvn package
-[INFO] Scanning for projects...
-[ERROR] [ERROR] Some problems were encountered while processing the POMs:
-[ERROR] Malformed POM /Users/matthewelbing/School/whs_planner/pom.xml: Unrecognised tag: 'plugin' (position: START_TAG seen ...</build>\n    <plugin>... @31:13)  @ /Users/matthewelbing/School/whs_planner/pom.xml, line 31, column 13
-[WARNING] 'dependencies.dependency.systemPath' for jfoenix:com.jfoenix:jar should not point at files within the project directory, ${project.basedir}/lib/jfoenix.jar will be unresolvable by dependent projects @ line 65, column 25
- @
-[ERROR] The build could not read 1 project -> [Help 1]
-[ERROR]
-[ERROR]   The project WHS_planner:whs_planner_app:1.0-SNAPSHOT (/Users/matthewelbing/School/whs_planner/pom.xml) has 1 error
-[ERROR]     Malformed POM /Users/matthewelbing/School/whs_planner/pom.xml: Unrecognised tag: 'plugin' (position: START_TAG seen ...</build>\n    <plugin>... @31:13)  @ /Users/matthewelbing/School/whs_planner/pom.xml, line 31, column 13 -> [Help 2]
-[ERROR]
-[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
-[ERROR] Re-run Maven using the -X switch to enable full debug logging.
-[ERROR]
-[ERROR] For more information about the errors and possible solutions, please read the following articles:
-[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/ProjectBuildingException
-[ERROR] [Help 2] http://cwiki.apache.org/confluence/display/MAVEN/ModelParseException
-Matthews-MacBook-Pro:whs_planner matthewelbing$ aa
+#!/usr/bin/env bash
