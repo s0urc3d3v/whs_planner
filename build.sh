e98d7d11535057d2ea2b65339c183e01697b7bd2
matthewelbing <author@techredesign.net>
2016-12-16 08:15:32 -0500

    fix build stuff

--- build.sh
+++ build.sh
@@ -2,7 +2,3 @@
 mvn clean compile assembly:single -DskipTests
 mvn package appbundle:bundle -DskipTests
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-printf "\n-----RUNNING JAR----\n"
-/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -jar $DIR/target/whs_planner_app-1.0-SNAPSHOT-jar-with-dependencies.jar
-$HOME/visualvm/bin/visualvm
-hdiutil attach $DIR/target/whs_planner_app-1.0-SNAPSHOT.dmg
