d1d8fb29c7c7defc21ff2c389dfa074515f37390
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2017-01-06 14:14:36 -0500

    clean.sh works now!!!!

--- clean.sh
+++ clean.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+DIR=${PWD##*/}
+rm -rf $DIR/src/main/resources/Schedule/output.html
+rm -rf $DIR/src/main/resources/Schedule/Schedule.json
+rm -rf $DIR/src/main/resources/Schedule/DayArray.json
 rm -rf Keys
-rm -rf
