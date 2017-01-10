e5bec198cd5163209234cd781c2aa8bc9e8eb844
tyler_brient <tyler_brient@student.wayland.k12.ma.us>
2017-01-10 12:10:45 -0500

    Fixed strikethrough from going to the edge

--- src/main/java/WHS_planner/Calendar/Task.java
+++ src/main/java/WHS_planner/Calendar/Task.java
@@ -1,10 +1,14 @@
 package WHS_planner.Calendar;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 
 /**
  * Created by geoffrey_wang on 9/20/16.
@@ -30,14 +34,16 @@ public class Task {
     }
 
     public Pane getPane(CalendarBox box){
-//        HBox pane = new HBox();
-        StackPane pane = new StackPane();
+        HBox pane = new HBox();
+//        StackPane pane = new StackPane();
 
         pane.setMinHeight(30);
         pane.setStyle("-fx-background-color:#c2d7f9;");
         pane.setAlignment(Pos.CENTER_LEFT);
-        Text label = new Text(Title + " " + Class + " " + Description);
-
+        Text label = new Text(Description);
+        Text spaces = new Text("  ");
+        label.setBoundsType(TextBoundsType.VISUAL);
+        pane.getChildren().add(spaces);
         pane.getChildren().add(label);
 
         pane.setOnMouseClicked((event -> {
