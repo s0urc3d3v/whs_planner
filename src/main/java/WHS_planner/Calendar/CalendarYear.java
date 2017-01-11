b199562db597d00c7e182ca25fbd1f8cbff3ea5a
Sharqosity <typingpalguy@gmail.com>
2017-01-11 18:46:13 -0500

    Replaced a bunch of borderpanes in schedule fxml.

--- src/main/java/WHS_planner/Calendar/CalendarYear.java
+++ src/main/java/WHS_planner/Calendar/CalendarYear.java
@@ -1,7 +1,11 @@
 package WHS_planner.Calendar;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+
+import java.io.InputStream;
 
 /**
  * Created by geoffrey_wang on 1/9/17.
@@ -14,6 +18,7 @@ public class CalendarYear extends Pane {
     public CalendarYear(){
         for (int i = 0; i < 12; i++) {
             JFXButton nextButton = new JFXButton(">");
+//            JFXButton nextButton = createIconButton("\uf054");
             nextButton.setOnMouseClicked(event -> {
                 if(month == 11) {
                     month = 0;
@@ -24,6 +29,7 @@ public class CalendarYear extends Pane {
             });
 
             JFXButton prevButton = new JFXButton("<");
+//            JFXButton prevButton = createIconButton("\uf053");
             prevButton.setOnMouseClicked(event -> {
                 if (month == 0) {
                     month = 11;
@@ -40,6 +46,32 @@ public class CalendarYear extends Pane {
         }
 
         this.getChildren().setAll(months[month]);
+    }
+
+    public static JFXButton createIconButton(String iconName) {
+        return createIconButton(iconName, "", 16);
+    }
+
+    public static JFXButton createIconButton(String iconName, String text, int iconSize) {
+        InputStream font = MainUI.class.getResourceAsStream("/FontAwesome/fontawesome.ttf");
+        Font.loadFont(font, 10);
+
+        Label icon = createIconLabel(iconName, 16);
+        icon.setStyle("-fx-font-size: " + iconSize + "px;");
+        JFXButton button = new JFXButton();
+        button.setGraphic(icon);
+        return button;
+    }
+
+    public static Label createIconLabel(String iconName, int iconSize) {
+
+        Label label = new Label();
+        label.setText(iconName);
+        label.getStyleClass().add("icons");
+        label.setStyle("-fx-font-size: " + iconSize + "px;");
+
+        return label;
+
     }
 
     private void changeMonth(int month) {
