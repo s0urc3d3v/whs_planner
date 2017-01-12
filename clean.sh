909d274d417d5a2e5b2315efacd64354fa9dd5ba
s0urc3d3v3l0pm3nt <github@techredesigned.com>
2017-01-12 14:01:57 -0500

    fix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 DIR=${PWD##*/}
-rm -rf $DIR/src/main/resources/Schedule/output.html
-rm -rf $DIR/src/main/resources/Schedule/Schedule.json
-rm -rf $DIR/src/main/resources/Schedule/DayArray.json
+rm -rf $DIR/src/main/resources/Schedule/json/*.json
+rm -rf $DIR/src/main/resources/Calendar/*.json
+rm -rf $DIR/Schedule.json
 rm -rf Keys
+
