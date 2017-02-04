9880a599e8f420433ef7ddaa28ffc59fe918d97e
Sharqosity <typingpalguy@gmail.com>
2017-02-03 22:47:32 -0500

    Added licences under about section

--- src/main/java/WHS_planner/UI/MainPane.java
+++ src/main/java/WHS_planner/UI/MainPane.java
@@ -15,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 
 import java.io.File;
 import java.io.IOException;
@@ -178,6 +179,21 @@ public class MainPane extends StackPane {
                 for (String name : names) {
                     info.getChildren().add(new Label(name));
                 }
+                info.getChildren().add(new Label("\n"));
+                JFXButton licences = new JFXButton("Licences...");
+                licences.setButtonType(JFXButton.ButtonType.RAISED);
+                licences.getStyleClass().setAll("gray-button");
+                licences.setOnMouseClicked(showLicences -> {
+                    info.getChildren().clear();
+                    Label licenceText = new Label("We used the JSOUP library (https://jsoup.org/) which is licensed under the MIT license:\nThe MIT License \nCopyright © 2009 - 2016 Jonathan Hedley (jonathan@hedley.net) \nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. \nWe also used federkasten's appbundler library (https://github.com/federkasten/appbundle-maven-plugin), as well as the JFoenix UI library (http://www.jfoenix.com/). \nBoth are avaiable under the Apache License 2.0(https://www.apache.org/licenses/LICENSE-2.0).");
+                    licenceText.setWrapText(true);
+                    licenceText.getStyleClass().addAll("times");
+                    info.getChildren().add(licenceText);
+
+
+                });
+
+                info.getChildren().add(licences);
                 info.getStyleClass().setAll("large-text");
                 info.setPadding(new Insets(10));
             });
